3c5dbabca68cc69c74ff27413bc1df2542ecdd5a
Christian Søtorp <>
2026-02-24 10:41:44 +0100

    Formatting

--- lua/alcensoft.lua
+++ lua/alcensoft.lua
@@ -1,11 +1,14 @@
 -- Setup watchman to help LSPs to figure out file changes
 require 'watchman'
 
-return { {
-  'juneedahamed/svnj.vim',
-}, {
-  'sainnhe/everforest',
-  config = function()
-    vim.cmd.colorscheme 'everforest'
-  end,
-} }
+return {
+  {
+    'juneedahamed/svnj.vim',
+  },
+  {
+    'sainnhe/everforest',
+    config = function()
+      vim.cmd.colorscheme 'everforest'
+    end,
+  },
+}
