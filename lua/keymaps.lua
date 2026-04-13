02da66b123187373c2e343bea711c64b6dc93f02
Christian Søtorp <>
2026-04-13 10:33:55 +0200

    Remove troublesome courserhold autocommand

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -67,10 +67,3 @@ vim.keymap.set('n', '<leader>L', function()
     height = 0.9,
   })
 end, { desc = 'Open lynx in floating terminal' })
-
--- Show errors and warnings in a floating window
-vim.api.nvim_create_autocmd('CursorHold', {
-  callback = function()
-    vim.diagnostic.open_float(nil, { focusable = true, source = 'if_many' })
-  end,
-})
