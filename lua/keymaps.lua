95710beb6a6ca622c4bcdc3ffa727ad0fa380e15
Christian Søtorp <>
2026-04-13 10:42:09 +0200

    Readd float, but without being focusable

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -67,3 +67,10 @@ vim.keymap.set('n', '<leader>L', function()
     height = 0.9,
   })
 end, { desc = 'Open lynx in floating terminal' })
+
+-- Show errors and warnings in a floating window
+vim.api.nvim_create_autocmd('CursorHold', {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focusable = false, source = 'if_many' })
+  end,
+})
