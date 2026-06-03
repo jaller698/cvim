2f747741959a8e9cfa7e587238c4d7e0631e12c3
Christian Søtorp <>
2026-06-03 13:14:13 +0200

    Fix copy + paste with ctrl in neovide

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -26,3 +26,17 @@ end, opts)
 vim.keymap.set({ 'n', 'i' }, '<C-BS>', function()
   ResetGuiFont()
 end, opts)
+
+local function save()
+  vim.cmd.write()
+end
+local function copy()
+  vim.cmd [[normal! "+y]]
+end
+local function paste()
+  vim.api.nvim_paste(vim.fn.getreg '+', true, -1)
+end
+
+vim.keymap.set({ 'n', 'i', 'v' }, '<C-s>', save, { desc = 'Save' })
+vim.keymap.set('v', '<C-c>', copy, { silent = true, desc = 'Copy' })
+vim.keymap.set({ 'n', 'i', 'v', 'c', 't' }, '<C-v>', paste, { silent = true, desc = 'Paste' })
